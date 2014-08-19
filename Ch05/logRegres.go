b72b09ee97e612b0d71e5ed0156222d11f7da4d2
Wang Bin <wangbin.zibo@gmail.com>
2014-08-19 14:19:33 +0800

    refactor logRegres.go, first example finished

--- Ch05/logRegres.go
+++ Ch05/logRegres.go
@@ -8,7 +8,74 @@ import (
 	"strconv"
 )
 
-func loadDataSet() (dataMat [][]float64, labelMat []float64) {
+type Array []float64
+
+func (this Array) Add(a Array) Array {
+	length := len(this)
+	result := make(Array, length)
+	for i, _ := range this {
+		result[i] = this[i] + a[i]
+	}
+	return result
+}
+
+func (this Array) Minus(a Array) Array {
+	length := len(this)
+	result := make(Array, length)
+	for i, _ := range this {
+		result[i] = this[i] - a[i]
+	}
+	return result
+}
+
+func (this Array) Multiple(a float64) Array {
+	length := len(this)
+	result := make(Array, length)
+	for i, _ := range this {
+		result[i] = this[i] * a
+	}
+	return result
+}
+
+func (this Array) Sigmoid() Array {
+	s := make(Array, 0)
+	for _, x := range this {
+		s = append(s, 1.0/(1+math.Exp(0-x)))
+	}
+	return s
+}
+
+type Matrix [][]float64
+
+func (this Matrix) Shape() (int, int) {
+	return len(this), len(this[0])
+}
+
+func (this Matrix) Multiple(weights Array) (result Array) {
+	m, n := this.Shape()
+	for i := 0; i < m; i++ {
+		sum := 0.0
+		for j := 0; j < n; j++ {
+			sum += this[i][j] * weights[j]
+		}
+		result = append(result, sum)
+	}
+	return
+}
+
+func (this Matrix) Transpose() Matrix {
+	m, n := this.Shape()
+	result := make(Matrix, n)
+	for i := 0; i < n; i++ {
+		result[i] = make([]float64, m)
+		for j := 0; j < m; j++ {
+			result[i][j] = this[j][i]
+		}
+	}
+	return result
+}
+
+func loadDataSet() (dataMat Matrix, labelMat []float64) {
 	file, err := os.Open("testSet.txt")
 	if err != nil {
 		panic(err)
@@ -44,35 +111,23 @@ func loadDataSet() (dataMat [][]float64, labelMat []float64) {
 	return
 }
 
-func sigmoid(x float64) float64 {
-	return 1.0 / (1 + math.Exp(0-x))
-}
-
-func gradAscent(dataMat [][]float64, labelMat []float64) (weights []float64) {
-	m := len(dataMat)
-	n := len(dataMat[0])
+func gradAscent(dataMat Matrix, labelMat Array) (weights Array) {
+	_, n := dataMat.Shape()
+	t := dataMat.Transpose()
 	alpha := 0.001
 	maxCycle := 500
 	for i := 0; i < n; i++ {
 		weights = append(weights, 1.0)
 	}
 	for i := 0; i < maxCycle; i++ {
-		for j := 0; j < m; j++ {
-			data := dataMat[j]
-			label := labelMat[j]
-			for k := 0; k < n; k++ {
-				h := sigmoid(data[k] * weights[k])
-				error := label - h
-				weights[k] += alpha * error
-			}
-		}
+		h := dataMat.Multiple(weights).Sigmoid()
+		error := labelMat.Minus(h)
+		weights = weights.Add(t.Multiple(error).Multiple(alpha))
 	}
 	return
 }
 
 func main() {
 	dataMat, labelMat := loadDataSet()
-	fmt.Println(dataMat)
-	fmt.Println(labelMat)
 	fmt.Println(gradAscent(dataMat, labelMat))
 }
